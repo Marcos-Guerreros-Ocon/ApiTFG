1d23df18652c2e8d7693f89ee65c32d04d780288
Marcos Guerreros Ocón <guerrerosoconm@gmail.com>
2023-01-16 14:37:07 +0100

    Subida API

---
+++ ApiProyecto/ApiProyecto/Controllers/GeneroController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiProyecto.DataAccess;
+using ApiProyecto.Models;
+
+namespace ApiProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneroController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public GeneroController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genero
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genero>>> GetGenero()
+        {
+            return await _context.Genero.ToListAsync();
+        }
+
+        // GET: api/Genero/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genero>> GetGenero(int id)
+        {
+            var genero = await _context.Genero.FindAsync(id);
+
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+        }
+
+        // PUT: api/Genero/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenero(int id, Genero genero)
+        {
+            if (id != genero.IdGenero)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genero).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GeneroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genero
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Genero>> PostGenero(Genero genero)
+        {
+            _context.Genero.Add(genero);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGenero", new { id = genero.IdGenero }, genero);
+        }
+
+        // DELETE: api/Genero/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenero(int id)
+        {
+            var genero = await _context.Genero.FindAsync(id);
+            if (genero == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genero.Remove(genero);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GeneroExists(int id)
+        {
+            return _context.Genero.Any(e => e.IdGenero == id);
+        }
+    }
+}
---
+++ ApiProyecto/ApiProyecto/Controllers/LibroController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiProyecto.DataAccess;
+using ApiProyecto.Models;
+
+namespace ApiProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LibroController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Libro
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> GetLibro()
+        {
+            return await _context.Libro.ToListAsync();
+        }
+
+        // GET: api/Libro/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Libro>> GetLibro(int id)
+        {
+            var libro = await _context.Libro.FindAsync(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            return libro;
+        }
+
+        // PUT: api/Libro/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLibro(int id, Libro libro)
+        {
+            if (id != libro.Isbn)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(libro).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LibroExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Libro
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Libro>> PostLibro(Libro libro)
+        {
+            _context.Libro.Add(libro);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LibroExists(libro.Isbn))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetLibro", new { id = libro.Isbn }, libro);
+        }
+
+        // DELETE: api/Libro/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLibro(int id)
+        {
+            var libro = await _context.Libro.FindAsync(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libro.Remove(libro);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LibroExists(int id)
+        {
+            return _context.Libro.Any(e => e.Isbn == id);
+        }
+    }
+}
